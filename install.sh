b39326c7e3ff04c2194fb9c205075f6551255e5c
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-03-04 02:08:47 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+# Download PaperMC server jar file
+wget https://api.papermc.io/v2/projects/paper/versions/1.19.3/builds/431/downloads/paper-1.19.3-431.jar
+# Download ngrok zip file
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+# Unzip ngrok file
+unzip ngrok-stable-linux-amd64.zip
