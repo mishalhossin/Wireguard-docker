9ae63adc47b048a40e1fe6d7ab79bab0c08988a7
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-03-04 03:06:44 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y wget
-wget -O jdk-8u221-linux-x64.tar.gz --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "https://download.oracle.com/otn-pub/java/jdk/8u221-b11/230deb18db3e4014bb8e3e8324f81b43/jdk-8u221-linux-x64.tar.gz"
-sudo mkdir /usr/lib/jvm
-sudo tar -xvzf jdk-8u221-linux-x64.tar.gz -C /usr/lib/jvm
-sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_221/bin/java" 1
-sudo update-alternatives --set java /usr/lib/jvm/jdk1.8.0_221/bin/java
-# Download PaperMC server jar file
-wget https://api.papermc.io/v2/projects/paper/versions/1.19.3/builds/431/downloads/paper-1.19.3-431.jar
-# Download ngrok zip file
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-# Unzip ngrok file
-unzip ngrok-stable-linux-amd64.zip
-# Run PaperMC server
-java -jar paper-1.19.3.jar
-# Run ngrok and expose port 25565
-ngrok tcp 25565
+
+# Update package lists and upgrade packages
+sudo apt-get update && sudo apt-get upgrade -y
+
+# Install WireGuard
+sudo apt-get install -y wireguard
+
+# Generate private and public keys for the server
+umask 077
+wg genkey | tee /etc/wireguard/privatekey | wg pubkey > /etc/wireguard/publickey
+
+# Generate private and public keys for the client
+wg genkey | tee client_privatekey | wg pubkey > client_publickey
+
+# Create the WireGuard configuration file for the server
+cat << EOF > /etc/wireguard/wg0.conf
+[Interface]
+Address = 10.0.0.1/24
+ListenPort = 51820
+PrivateKey = $(cat /etc/wireguard/privatekey)
+
+[Peer]
+PublicKey = $(cat client_publickey)
+AllowedIPs = 10.0.0.2/32
+EOF
+
+# Create the WireGuard configuration file for the client
+cat << EOF > wg0-client.conf
+[Interface]
+Address = 10.0.0.2/24
+PrivateKey = $(cat client_privatekey)
+
+[Peer]
+PublicKey = $(cat /etc/wireguard/publickey)
+Endpoint = SERVER_PUBLIC_IP:51820
+AllowedIPs = 0.0.0.0/0, ::/0 
+EOF
+
+echo "Client configuration saved to wg0-client.conf"
+
+# Enable IP forwarding on the server
+echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+# Start the WireGuard interface and enable it to start on boot
+sudo wg-quick up wg0 && sudo systemctl enable wg-quick@wg0.service
+
+echo "WireGuard server installed successfully!"
